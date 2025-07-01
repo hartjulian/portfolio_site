81b235d7f26bc622c3261935c68874bc8e7715cf
Julian Hart <julian.w.hart@gmail.com>
2025-07-01 19:18:40 +1200

    Changed navigation to collapse to burger menu on smaller screens

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function displayList() {
+function displayMenu() {
     document.getElementById("nav-options").classList.toggle("show");
 };
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.nav-btn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
